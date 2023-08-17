ba961e7f1f001b79f39151a6612c63f94a7bc6cb
CesarArredondo1 <cesar.arredondogarcia01@gmail.com>
2023-08-17 21:45:13 +0000

    studying for logic

---
+++ iftest/conditional01.py
@@ -0,0 +1,8 @@
+#!/bin/env python3
+
+# create the string host
+hostname= "MTG"
+# test logic with the 'if' stament
+# what to do if this statment is found to be true
+if hostname =="MTG":
+    print("The hostname was found to be MTG")
