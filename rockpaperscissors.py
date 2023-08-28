250992a83fd32bc8468d246e424b87a5af7bf005
CesarArredondo1 <cesar.arredondogarcia01@gmail.com>
2023-08-28 07:08:38 +0000

    RockPaperScissors

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,7 +4,7 @@
 import random
 
 while(True):
-     player_one = input("Please enter rock(r), paper(p), scissors(s): ")
+     player_one = input("Please enter rock[r], paper[p],scisssors[s]: ")
      player_two = random.randint(1,3)
 
 if player_two == 1 :
@@ -21,28 +21,25 @@ if (player_one == player_two):
 elif player_one == "r" :
      if player_two == "s":
          print("Player_one wins! Rock beats Scissors")
-                 else :
+     else:
                  print("Player_two wins!")
 
 
- elif player_one == "p":
+elif player_one == "p":
      if player_two == "r":
          print("Player_two wins! Paper beats Rock!")
-                 else:
+     else:
                  print("Player_one wins!")
 
- elif player_one =="s":
+elif player_one =="s":
      if player_two =="p":
          print("Player_one wins! Scissors beats Paper!")
-                 else:
+     else:
                  print("Player_two wins!")
 
 
-print("Youe choice: " + player_one "\nplayer_two's choice: " + player_two + "\nThank you fore playing!")
-print("Do you want to play again?  (Y/N)")
+print("Your choice: " + player_one + "\n player_two's choice: " + player_two + "\nThank you fore playing!")
+print("Do you want to play again? (Y/N)")
 ans = input()
-# if user input n or N then conditions is True
 if ans == 'n' or ans == 'N':
-    break
-
-
+    sys.exit()
